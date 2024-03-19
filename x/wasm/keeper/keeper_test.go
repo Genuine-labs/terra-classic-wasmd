7a3492475a6bc56ce4cca720070a073584b0e88c
Dong Lieu <duongduydongtq@gmail.com>
2024-03-19 15:25:13 +0700

    fix gas

--- x/wasm/keeper/keeper_test.go
+++ x/wasm/keeper/keeper_test.go
@@ -418,7 +418,7 @@ func TestInstantiate(t *testing.T) {
 
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x1b5bd), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x1b581), gasAfter-gasBefore)
 	}
 
 	// ensure it is stored properly
